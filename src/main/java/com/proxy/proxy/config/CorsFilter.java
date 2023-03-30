d6e378f251fef29fed32d2fcd44bdb080afd6eee
Денис Браусов <brausowdenis@gmail.com>
2023-03-30 11:50:14 +0300

    add to cors filter withCredentials

--- src/main/java/com/proxy/proxy/config/CorsFilter.java
+++ src/main/java/com/proxy/proxy/config/CorsFilter.java
@@ -16,6 +16,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, x-www-form-urlencoded, X-Requested-With, Content-Type, Accept," +
