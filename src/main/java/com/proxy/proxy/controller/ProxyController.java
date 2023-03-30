e9eec0e7c5342dc8a02935970f73b469b96ae2f1
Денис Браусов <brausowdenis@gmail.com>
2023-03-30 09:17:31 +0300

    Initial commit

---
+++ src/main/java/com/proxy/proxy/controller/ProxyController.java
@@ -0,0 +1,55 @@
+package com.proxy.proxy.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
+
+@RestController
+public class ProxyController {
+
+    @RequestMapping("/**")
+    public ResponseEntity mirrorRest(@RequestBody(required = false) String body,
+                                     HttpMethod method, HttpServletRequest request, HttpServletResponse response)
+            throws URISyntaxException {
+        String requestUrl = request.getRequestURI();
+        int port = 55052;
+        String server = "134.17.25.94";
+        String userInfo = "YURY:2195662Aa";
+        URI uri = new URI("http", userInfo, server, port, null, null, null);
+        uri = UriComponentsBuilder.fromUri(uri)
+                .path(requestUrl)
+                .query(request.getQueryString())
+                .build(true).toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            headers.set(headerName, request.getHeader(headerName));
+        }
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            System.out.println("Proxy URL to: " + uri);
+            return restTemplate.exchange(uri, method, httpEntity, String.class);
+        } catch(HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getRawStatusCode())
+                    .headers(e.getResponseHeaders())
+                    .body(e.getResponseBodyAsString());
+        }
+    }
+}
