f98e320f951454be0e76d1c30a50b4bab3a6ec2b
Денис Браусов <brausowdenis@gmail.com>
2023-03-30 12:14:05 +0300

    add to cors filter withCredentials

--- src/main/java/com/proxy/proxy/config/CorsFilter.java
+++ src/main/java/com/proxy/proxy/config/CorsFilter.java
@@ -11,19 +11,12 @@ import java.util.Arrays;
 @Component
 public class CorsFilter implements Filter {
 
-    private String[] url = {
-            "https://hailrase.github.io/greeting-oktell",
-            "https://proxy-tcyj.onrender.com",
-            "http://134.17.25.94:55052",
-            "https://hailrase.github.io"
-    };
-
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", Arrays.toString(url));
+        response.setHeader("Access-Control-Allow-Origin", "https://hailrase.github.io");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
